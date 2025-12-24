7e3b200497724e4de666df96a7bd49debc2dd485
max <maxwellcosta2009@gmail.com>
2025-12-24 00:01:16 -0500

    Update colorDetectorTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorDetectorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorDetectorTest.java
@@ -99,7 +99,7 @@ public class colorDetectorTest extends LinearOpMode {
 
     private void initializeAndSetUp() {
        // colorDetector.enableLed(true);
-        
+
         hardwareMapping();
     }
 
